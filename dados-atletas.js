b5b563d7bf40f30debd6eab6362558055bcbfbf0
Carlos230507 <118301831+Carlos230507@users.noreply.github.com>
2022-11-15 17:28:29 -0300

    Update dados-atletas.js

--- dados-atletas.js
+++ dados-atletas.js
@@ -9,47 +9,63 @@ this.notas = notas
   
 calculaCategoria() {
 if (this.idade > 8 && this.idade < 12) {
-return "Infantil"}
+return "Infantil"
+}
 if (this.idade = 12 || this.idade = 13) {
-return "Juvenil"}
+return "Juvenil"
+}
 if (this.idade = 14 || this.idade = 15) {
-return "Intermediário"}
+return "Intermediário"
+}
 if (this.idade > 15 && this.idade < 31) {
-return "Adulto"}
-else {return "Sem categoria"}
+return "Adulto"
+} else {
+  return "Sem categoria"
+}
 }
   
 calculaIMC() {
 let altura = this.altura * this.altura
-return this.peso / altura}
+return this.peso / altura
+}
   
 calculaMediaValida() {
 this.notas = this.notas.sort()
 let notasValidas = this.notas.slice(1, 4)
-return notasValidas / this.notas.length}
+return notasValidas / this.notas.length
+}
   
 obtemNomeAtleta() {
-return this.nome}
+return this.nome
+}
   
 obtemIdadeAtleta() {
-return this.idade}
+return this.idade
+}
   
 obtemPesoAtleta() {
-return this.peso}
+return this.peso
+}
   
 obtemNotasAtleta() {
-return this.notas}
+return this.notas
+}
   
 obtemCategoria() {
 if (this.idade > 8 && this.idade < 12) {
-return "Infantil"}
+return "Infantil"
+}
 if (this.idade = 12 || this.idade = 13) {
-return "Juvenil"}
+return "Juvenil"
+}
 if (this.idade = 14 || this.idade = 15) {
-return "Intermediário"}
+return "Intermediário"
+}
 if (this.idade > 15 && this.idade < 31) {
-return "Adulto"}
-else {return "Sem categoria"}
+return "Adulto"
+} else {
+  return "Sem categoria"
+}
 }
   
 obtemIMC() {
@@ -59,4 +75,5 @@ return this.peso / altura
 obtemMediaValida() {
 this.notas = this.notas.sort()
 let notasValidas = this.notas.slice(1, 4)
-return notasValidas / this.notas.length}
+return notasValidas / this.notas.length
+}
